06e512b42a772ad7d8bdfc38788f1cb54b84fa4d
nuk <kunnsh@gmail.com>
2022-07-02 08:39:33 +0800

    feat: add pwa support

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import App from "./App.vue";
 import "@/styles/global.less";
 import { createPinia } from "pinia";
 import VConsole from "vconsole";
-import { registerSW } from "virtual:pwa-register";
+import { useRegisterSW } from "virtual:pwa-register/vue";
 
 import.meta.env.DEV && new VConsole();
+
+
+useRegisterSW()
 createApp(App).use(createPinia()).mount("#app");
 
-const updateSW = registerSW({
-  onNeedRefresh() {},
-  onOfflineReady() {},
-});
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,39 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({})],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Simple Todo",
+        short_name: "SimpleTodo",
+        description: "Todo App made by vue3",
+        theme_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: ".",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
