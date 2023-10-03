08813c07c804e0553717f5b1c36d76fc59b1673e
nuk <kunnsh@gmail.com>
2023-10-03 16:50:09 +0800

    feat: add left panel

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import '@/styles/global.less'
+import '@/styles/global.scss'
 import { createPinia } from 'pinia'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
+import GithubBadge from '@/components/github-badge/github-badge.vue'
 
 // import.meta.env.DEV && new VConsole();
 
 useRegisterSW()
-createApp(App).use(createPinia()).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+
+app.component('GithubBadge', GithubBadge)
+
+app.mount('#app')
