b13dac0d8ec6ce27344ce6c83873fc59229be5cc
nuk <kunnsh@gmail.com>
2023-10-03 13:52:05 +0800

    feat: update npm version

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Simple Todo
 
-build with vue3 + typescript + pinia
-
-在线体验地址：http://simple-todo-phi.vercel.app/
+在线地址：http://simple-todo-phi.vercel.app/
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "@/styles/global.less";
-import { createPinia } from "pinia";
-import VConsole from "vconsole";
-import { useRegisterSW } from "virtual:pwa-register/vue";
+import '@/styles/global.less'
+import { createPinia } from 'pinia'
+import { useRegisterSW } from 'virtual:pwa-register/vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import.meta.env.DEV && new VConsole();
-
+// import.meta.env.DEV && new VConsole();
 
 useRegisterSW()
-createApp(App).use(createPinia()).mount("#app");
-
+createApp(App).use(createPinia()).mount('#app')
