0f2c574b574b00173145737727dbbb86cf5a7fdc
nuk <kunnsh@gmail.com>
2023-11-17 17:39:44 +0800

    fix: add delete confirm

--- src/components/todo-item/todo-item.vue
+++ src/components/todo-item/todo-item.vue
@@ -4,14 +4,33 @@
       <div class="text">{{ item.text }}</div>
       <div class="time">{{ formatDate(item.createAt) }}</div>
     </div>
-    <div>
-      <svg @click="$emit('deleteItem', item)" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--material-symbols"
-        width="32" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-        <path fill="currentColor"
-          d="m12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z">
-        </path>
-      </svg>
+    <div class="flex">
+      <div class="mr-2">
+        <svg t="1700213550873" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+          p-id="2438" width="16" height="16">
+          <path fill="currentColor"
+            d="M257.7 752c2 0 4-0.2 6-0.5L431.9 722c2-0.4 3.9-1.3 5.3-2.8l423.9-423.9c3.9-3.9 3.9-10.2 0-14.1L694.9 114.9c-1.9-1.9-4.4-2.9-7.1-2.9s-5.2 1-7.1 2.9L256.8 538.8c-1.5 1.5-2.4 3.3-2.8 5.3l-29.5 168.2c-1.9 11.1 1.5 21.9 9.4 29.8 6.6 6.4 14.9 9.9 23.8 9.9z m67.4-174.4L687.8 215l73.3 73.3-362.7 362.6-88.9 15.7 15.6-89zM880 836H144c-17.7 0-32 14.3-32 32v36c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-36c0-17.7-14.3-32-32-32z"
+            p-id="2439"></path>
+        </svg>
+      </div>
+      <div class="mr-2">
+        <svg t="1700213268878" class="icon" width="16" height="16" viewBox="0 0 1024 1024" version="1.1"
+          xmlns="http://www.w3.org/2000/svg" p-id="1457">
+          <path fill="currentColor"
+            d="M212 338c-24.852 0-45-20.148-45-45S187.148 248 212 248h600c24.852 0 45 20.148 45 45S836.852 338 812 338H212z m0 220c-24.852 0-45-20.148-45-45S187.148 468 212 468h600c24.852 0 45 20.148 45 45S836.852 558 812 558H212z m0 220c-24.852 0-45-20.148-45-45S187.148 688 212 688h600c24.852 0 45 20.148 45 45S836.852 778 812 778H212z"
+            p-id="1458"></path>
+        </svg>
+      </div>
+      <div>
+        <svg @click="deleteItem(item)" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true" role="img" class="iconify iconify--material-symbols" width="16" height="16"
+          preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+          <path fill="currentColor"
+            d="m12 13.4l-4.9 4.9q-.275.275-.7.275q-.425 0-.7-.275q-.275-.275-.275-.7q0-.425.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7q0-.425.275-.7q.275-.275.7-.275q.425 0 .7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275q.425 0 .7.275q.275.275.275.7q0 .425-.275.7L13.4 12l4.9 4.9q.275.275.275.7q0 .425-.275.7q-.275.275-.7.275q-.425 0-.7-.275Z">
+          </path>
+        </svg>
+      </div>
+
     </div>
   </div>
 </template>
@@ -21,10 +40,17 @@ import type { Todo } from "@/types/todo";
 import * as timeago from 'timeago.js';
 
 const { item } = defineProps<{ item: Todo }>();
+const emit = defineEmits();
 const store = useStore();
 
 function formatDate(createdAt: string) {
   return timeago.format(new Date(createdAt));
+}
+
+function deleteItem(item: Todo) {
+  if (confirm('delete this item?')) {
+    emit('deleteItem', item)
+  }
 }
 </script>
 <style scoped lang="less">
